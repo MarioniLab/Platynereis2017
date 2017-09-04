02df5e6fd9fdbe62614bec8251f427abf7e8ab6c
nilseling <nils.eling@gmx.de>
2017-09-04 11:34:00 +0100

    Added resequenced empty wells

--- Preprocessing/QC/Quality_control.R
+++ Preprocessing/QC/Quality_control.R
@@ -10,6 +10,8 @@ setwd("/Users/eling01/Google Drive/")
 
 # Read in metadata on cells
 QC <- as.data.frame(read_xlsx("Platy/6th_approach/Data/QC/metadata.xlsx"))
+# Now with extra empty wells
+QC <- as.data.frame(read.csv("Platy/6th_approach/Data/QC/metadata_withempty.xlsx"))
 
 # Read in the transcript counts
 raw.data <- read.table("Platy/6th_approach/Data/Raw/170502_annotGenes.txt", sep = "\t", header = TRUE)
@@ -19,7 +21,7 @@ raw.data <- raw.data[,colnames(raw.data) %in% as.character(QC$SampleID)]
 QC <- QC[as.character(QC$SampleID) %in% colnames(raw.data),]
 
 # Fig S1A - Plot number of mapped reads, color by empty well, single of multiple
-plot(log10(colSums(raw.data)), pch = 16, col = ifelse(as.numeric(QC$nr.of.cells) == 1 & is.na(QC$note), "goldenrod", ifelse(as.numeric(QC$nr.of.cells) > 1 & is.na(QC$note), "darkgreen", ifelse(as.numeric(QC$nr.of.cells) == 0 & is.na(QC$note), "darkblue", "black"))))
+plot(log10(colSums(raw.data[!grepl("ERCC-", rownames(raw.data)),])), pch = 16, col = ifelse(as.numeric(QC$nr.of.cells) == 1 & is.na(QC$note), "goldenrod", ifelse(as.numeric(QC$nr.of.cells) > 1 & is.na(QC$note), "darkgreen", ifelse(as.numeric(QC$nr.of.cells) == 0 & is.na(QC$note), "darkblue", "black"))))
 
 # Select single cells 
 single <- as.character(QC$SampleID[QC$nr.of.cells == 1 & is.na(QC$note)])
